58971380908bf8211b6bfda473f57af98cc44e7f
Farrel2500 <122343284+Farrel2500@users.noreply.github.com>
2023-02-28 14:59:50 +0700

    Add files via upload

---
+++ Perhitungan.py
@@ -0,0 +1,5 @@
+from PKG
+nilai.nilai(20, 40)
+
+hasil = penjumlahan.kali(2, 6)
+print(hasil)
---
+++ paket.py
@@ -0,0 +1,12 @@
+from PKG.Modul1 import *
+from PKG.Modul2 import *
+from PKG.Hitungnilai import hitung_nilai,hitung_nilai2
+
+def main():
+    f1()
+    f2()
+    f3()
+    f4()
+    print("Nilai akhir :",hitung_nilai(58,86))
+    print("Nilai akhir2 :",hitung_nilai2(50,50))
+main()
